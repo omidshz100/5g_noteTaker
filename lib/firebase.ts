daec775fc53c8e07fab840804e82f1d3652a2534
Omid Shojaeian Zanjani <46863450+omidshz100@users.noreply.github.com>
2026-06-19 20:23:34 +0200

    fix: prevent Firebase prerender error, add dynamic rendering

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -11,6 +11,10 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+const app = getApps().length === 0
+  ? initializeApp(firebaseConfig)
+  : getApps()[0];
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
